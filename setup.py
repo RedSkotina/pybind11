039772998db9e8b6a9bb0b524bc5567d6b302c0b
Jason Rhinelander <jason@imaginary.ca>
2017-04-20 16:01:19 -0400

    Allow binding factory functions as constructors
    This allows you to use:
    
        cls.def(py::init_factory(&factory_function));
    
    where `factory_function` is some pointer, holder, value, or
    handle-generating factory function of the type that `cls` binds.
    
    Various compile-time checks are performed to ensure the function is
    valid, and various run-time type checks where necessary (i.e. when using
    a python-object-returning function, or when a dynamic_cast is needed
    for downcasting a pointer).
    
    The feature is optional, and requires including the <pybind11/factory.h>
    header.

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ else:
         'include/pybind11/descr.h',
         'include/pybind11/eigen.h',
         'include/pybind11/eval.h',
+        'include/pybind11/factory.h',
         'include/pybind11/functional.h',
         'include/pybind11/numpy.h',
         'include/pybind11/operators.h',
